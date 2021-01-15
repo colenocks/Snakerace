ae88aca3117be1587111f09c5cfdb2e6bf30ed08
Cole Enocks <enockscoleman@gmail.com>
2021-01-15 00:14:57 +0000

    add logout handler

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import Home from "./components/Home/Home";
 import Arena from "./components/Arena/Arena";
-import { clientRequest } from "./AxiosConfig";
+import EnterGame from "./components/EnterGame/EnterGame";
+import { clientRequest, getBaseURL } from "./AxiosConfig";
+import socketIOClient from "socket.io-client";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import "./App.scss";
@@ -17,6 +19,7 @@ class App extends Component {
     this.state = {
       is_logged_in: false,
       user_session: null,
+      username: null,
     };
 
     this.loginHandler = this.loginHandler.bind(this);
@@ -27,11 +30,13 @@ class App extends Component {
   componentDidMount() {
     const is_logged_in = localStorage.getItem("is_logged_in");
     const user_session = localStorage.getItem("user_session");
+    const username = localStorage.getItem("username");
 
     if (is_logged_in || user_session) {
       this.setState({
         is_logged_in,
         user_session,
+        username,
       });
     }
   }
@@ -46,12 +51,14 @@ class App extends Component {
         });
         localStorage.setItem("is_logged_in", true);
         localStorage.setItem("user_session", res.data.user_session);
+        localStorage.setItem("username", res.data.username);
         this.setState(
           {
             is_logged_in: true,
             user_session: res.data.user_session,
+            username: res.data.username,
           },
-          () => this.props.history.push("/arena")
+          () => this.props.history.push("/entergame")
         );
       })
       .catch((err) => {
@@ -80,14 +87,28 @@ class App extends Component {
   }
 
   logoutHandler() {
-    this.setState({
-      user_session: null,
-      is_logged_in: false,
-    });
+    const url = getBaseURL();
+    const socket = socketIOClient(url);
 
-    localStorage.clear();
-    this.props.history.push("/");
-    toast("You have logged out successfully", { type: "warning" });
+    clientRequest()
+      .get("/logout")
+      .then((res) => {
+        console.log(res);
+        toast(res.data.message, { type: "dark" });
+        this.setState({
+          user_session: null,
+          is_logged_in: false,
+        });
+        localStorage.clear();
+        socket.disconnect();
+        this.props.history.push("/");
+      })
+      .catch((err) => {
+        console.error("Signup Error: " + err);
+        this.setState({
+          is_logged_in: false,
+        });
+      });
   }
 
   render() {
@@ -103,7 +124,7 @@ class App extends Component {
             <Route
               exact
               path='/'
-              render={(props) => (
+              render={() => (
                 <Home
                   loginHandler={this.loginHandler}
                   signupHandler={this.signupHandler}
@@ -113,7 +134,24 @@ class App extends Component {
             <Route
               exact
               path='/arena'
-              render={() => <Arena user_session={this.state.user_session} />}
+              render={(props) => (
+                <Arena
+                  {...props}
+                  user_session={this.state.user_session}
+                  username={this.state.username}
+                />
+              )}
+            />
+            <Route
+              exact
+              path='/entergame'
+              render={(props) => (
+                <EnterGame
+                  {...props}
+                  user_session={this.state.user_session}
+                  username={this.state.username}
+                />
+              )}
             />
           </Switch>
         </section>
